9ce33045f2e1ef432d87054cb751eda34bca22df
saul19bautista <lopezbautistasaul12@gmail.com>
2026-03-29 13:56:46 -0600

    fix: correcion del cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# app/main.py
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,43 +14,39 @@ from app.routers.admin_chapters import router as admin_chapters_router
 from app.routers.admin_dictamenes import router as admin_dictamenes_router
 from app.routers.editorial_chapters import router as editorial_chapters_router
 from app.routers.editorial_actions import router as editorial_actions_router
-
-# ✅ ESTE ES EL ÚNICO QUE DEBE QUEDAR PARA DICTAMINADOR
 from app.routers.dictaminador_chapters import router as dictaminador_chapters_router
-
 from app.routers.admin_chapter_versions import router as admin_chapter_versions_router
 from app.routers.admin_dictamen_documento import router as admin_dictamen_documento_router
 from app.routers.admin_templates import router as admin_templates_router
 
-import app.models  # asegura que se registren modelos
+import app.models
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+
+    "https://editorial-ten.vercel.app",
+    "https://frontend-6whf-git-main-luz1727s-projects.vercel.app",
+    "https://editorial-git-main-interpecdevs-projects.vercel.app",
+    "https://editorial-8s5chffq0-interpecdevs-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-
-        # ✅ SOLO ORIGIN (sin /login)
-        "https://editorial-ten.vercel.app",
-        "editorial-git-main-interpecdevs-projects.vercel.app",
-        "editorial-8s5chffq0-interpecdevs-projects.vercel.app",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ✅ CARPETAS
 os.makedirs("storage/convocatorias", exist_ok=True)
 os.makedirs("storage/chapters", exist_ok=True)
 os.makedirs("storage/dictamenes", exist_ok=True)
 
-# ✅ ESTÁTICOS (solo uno)
 app.mount("/api/storage", StaticFiles(directory="storage"), name="storage")
 
-# ✅ ROUTERS
 app.include_router(auth_router, prefix="/api")
 app.include_router(dashboard_router, prefix="/api")
 app.include_router(convocatorias_router, prefix="/api")
@@ -63,10 +58,7 @@ app.include_router(admin_chapters_router, prefix="/api")
 app.include_router(admin_dictamenes_router, prefix="/api")
 app.include_router(editorial_chapters_router, prefix="/api")
 app.include_router(editorial_actions_router, prefix="/api")
-
-# ✅ Dictaminador (solo este)
 app.include_router(dictaminador_chapters_router, prefix="/api")
-
 app.include_router(admin_chapter_versions_router, prefix="/api")
 app.include_router(admin_dictamen_documento_router, prefix="/api")
 app.include_router(admin_templates_router, prefix="/api")
